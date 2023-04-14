70239adf871243361feb8d5e65189a5239ca4616
Jacqueline Park <jacky.park@kellogg.northwestern.edu>
2023-04-14 21:29:52 +0000

    inclass assignment 2

--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,39 @@
 
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS comments;
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    first_name TEXT,
+    last_name TEXT,
+    location TEXT
+);
+
+CREATE TABLE posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_at TEXT,
+    image_url TEXT,
+    body TEXT
+    user_id INTEGER
+);
+
+CREATE TABLE likes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    post_id INTEGER
+    user_id INTEGER
+);
+
+CREATE TABLE comments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    body TEXT
+    post_id INTEGER
+    user_id INTEGER
+);
+
+CREATE TABLE followings (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+)
